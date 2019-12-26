de9ee1eaf4fe55cf2b03d417b9485e30acecfed2
Silvestro <csilvestro@csilvestrombp.fullsail.com>
2019-12-26 17:28:23 -0500

    Added Counter.

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import Counter from './components/Counter';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const initialState = {
+  count: 0
+};
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Counter />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
